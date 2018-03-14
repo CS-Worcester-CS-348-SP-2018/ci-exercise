ec368f6f7c9ccf15de5872ef14cf8fcf9662ff99
Ben Anderson <Ben Anderson>
2018-03-14 13:22:38 -0400

    Add-name fix 2

--- src/main/java/CS_348_SP_2018/CIExercise/EndsWith3.java
+++ src/main/java/CS_348_SP_2018/CIExercise/EndsWith3.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package CS_348_SP_2018.CIExercise;
 
 public class EndsWith3 {
@@ -16,26 +15,4 @@ public class EndsWith3 {
 	public String toString() {
 		return "EndsWith3 [names=" + names + "]";
 	}
-
 }
-=======
-package CS_348_SP_2018.CIExercise;
-
-public class EndsWith3 {
-	
-	String names;
-
-	public EndsWith3()  {
-		names= "";
-		names+= "Nathan Posterro\n";
-		// add your name here in the form of a line that looks like:
-		// names+= "John Smith\n";
-	}
-
-	@Override
-	public String toString() {
-		return "EndsWith3 [names=" + names + "]";
-	}
-
-}
->>>>>>> 1a2024da97644e004744212e3e788a09131c22ee
