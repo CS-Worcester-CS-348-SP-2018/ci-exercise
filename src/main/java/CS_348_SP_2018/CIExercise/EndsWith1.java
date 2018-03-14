29324518c927bcfcd58ba70ab51771401dde83d7
qphan1992 <qphan1@worcester.edu>
2018-03-14 14:15:39 -0400

    Added name v2

--- src/main/java/CS_348_SP_2018/CIExercise/EndsWith1.java
+++ src/main/java/CS_348_SP_2018/CIExercise/EndsWith1.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package CS_348_SP_2018.CIExercise;
 
 public class EndsWith1 {
@@ -6,7 +5,7 @@ public class EndsWith1 {
 	String names;
 
 	public EndsWith1()  {
-		names = "";
+		names = "Quoc Phan";
 		// add your name here in the form of a line that looks like:
 		// names+= "John Smith\n";
 	}
@@ -17,23 +16,3 @@ public class EndsWith1 {
 	}
 
 }
-=======
-package CS_348_SP_2018.CIExercise;
-
-public class EndsWith1 {
-	
-	String names;
-
-	public EndsWith1()  {
-		names = "Quoc Phan";
-		// add your name here in the form of a line that looks like:
-		// names+= "Quoc Phan\n";
-	}
-
-	@Override
-	public String toString() {
-		return "EndsWith1 [names=" + names + "]";
-	}
-
-}
->>>>>>> 1a2024da97644e004744212e3e788a09131c22ee
--- src/test/java/CS_348_SP_2018/CIExercise/EndsWith1Test.java
+++ src/test/java/CS_348_SP_2018/CIExercise/EndsWith1Test.java
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
-package CS_348_SP_2018.CIExercise;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-public class EndsWith1Test {
-
-	@Test
-	public void testToString() {
-		EndsWith1 one = new EndsWith1();
-		assertEquals("EndsWith1 [names=]", one.toString());
-	}
-
-}
-=======
 package CS_348_SP_2018.CIExercise;
 
 import static org.junit.Assert.*;
@@ -30,4 +14,3 @@ public class EndsWith1Test {
 	}
 
 }
->>>>>>> 1a2024da97644e004744212e3e788a09131c22ee
